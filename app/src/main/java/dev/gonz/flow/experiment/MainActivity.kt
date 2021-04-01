ea4eab5cfd0832dd50b73ce4c6bae31da6488310
Gonzalo Campos <erick.campos@wizeline.com>
2021-04-01 17:40:06 -0600

    Weird stuff.

--- app/src/main/java/dev/gonz/flow/experiment/MainActivity.kt
+++ app/src/main/java/dev/gonz/flow/experiment/MainActivity.kt
@@ -1,11 +1,54 @@
 package dev.gonz.flow.experiment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import dev.gonz.flow.experiment.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var myFlow: Flow<Int>
+    private var flowJob: Job? = null
+
+    @ExperimentalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+
+        myFlow = flow {
+            var counter = 1
+            while (true) {
+                emit(counter++)
+                if (counter > 10_000) {
+                    break
+                }
+                delay(10L)
+            }
+        }
+
+        binding.fab.clicksFlow()
+                .onEach { launchFlow() }
+                .launchIn(lifecycleScope)
+    }
+
+    private fun launchFlow() {
+        if (flowJob != null && flowJob!!.isActive) {
+            flowJob?.cancel()
+        }
+        flowJob = myFlow.flowOn(Dispatchers.Default)
+                .onEach { number ->
+                    binding.tvCounter.text = number.toString()
+                }
+                .onCompletion {
+                    Toast.makeText(this@MainActivity, "Flow was finished!", Toast.LENGTH_SHORT).show()
+                }
+                .launchIn(lifecycleScope)
     }
 }
